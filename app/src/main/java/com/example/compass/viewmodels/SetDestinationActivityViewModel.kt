60db3917844fa36d7cb26c71ad816ff24442f981
Marcin Gruchała <margruchala@gmail.com>
2021-08-15 11:57:31 +0200

    Added screen rotation handling in SetDestinationActivity

--- app/src/main/java/com/example/compass/viewmodels/SetDestinationActivityViewModel.kt
+++ app/src/main/java/com/example/compass/viewmodels/SetDestinationActivityViewModel.kt
@@ -1,6 +1,9 @@
 package com.example.compass.viewmodels
 
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.AndroidViewModel
+import com.example.compass.R
 import com.example.compass.models.GeoLocation
 import com.example.compass.repository.Repository
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -8,12 +11,19 @@ import javax.inject.Inject
 
 @HiltViewModel
 class SetDestinationActivityViewModel @Inject constructor(
+    application: Application,
     private val repository: Repository
-) : ViewModel() {
+) : AndroidViewModel(application) {
+
+    var messageText = application.getString(
+        R.string.destination_input_description
+    )
+
+    var messageColor = ContextCompat.getColor(
+        application.applicationContext,
+        R.color.main_green)
 
     fun updateRepositoryWithDestination(newGeoLocation: GeoLocation) {
         repository.destination.value = newGeoLocation
     }
-
-
 }
--- app/src/main/java/com/example/compass/views/SetDestinationActivity.kt
+++ app/src/main/java/com/example/compass/views/SetDestinationActivity.kt
@@ -1,7 +1,6 @@
 package com.example.compass.views
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
@@ -17,17 +16,17 @@ import dagger.hilt.android.AndroidEntryPoint
 class SetDestinationActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySetDestinationBinding
     private val viewModel: SetDestinationActivityViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySetDestinationBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        setupSetDestinationDescription()
         setupClickListeners()
     }
 
     private fun setupClickListeners() {
         binding.btnSubmit.setOnClickListener {
-            Log.d("SetDestinationActivity", "Lat: ${binding.etLat.text} Lon: ${binding.etLon.text}")
             val inputValidation = DestinationInputValidator.validateDestinationInput(
                 binding.etLat.text.toString(),
                 binding.etLon.text.toString()
@@ -41,33 +40,36 @@ class SetDestinationActivity : AppCompatActivity() {
                 )
                 finish()
             } else {
-                handleInputError(inputValidation)
+                showInputErrorMessage(inputValidation)
             }
         }
-
         binding.btnGoBack.setOnClickListener {
             finish()
         }
     }
 
-    private fun handleInputError(inputStatus: DestinationInputStatus) {
+    private fun setupSetDestinationDescription() {
+        binding.tvSetDestinationDescription.text = viewModel.messageText
+        binding.tvSetDestinationDescription.setTextColor(viewModel.messageColor)
+    }
+
+    private fun showInputErrorMessage(inputStatus: DestinationInputStatus) {
         when(inputStatus) {
             DestinationInputStatus.OUT_OF_RANGE -> {
-                binding.tvSetDestinationDescription.text = getString(
-                    R.string.destination_input_status_outOfRange_message
-                )
-                binding.tvSetDestinationDescription.setTextColor(
-                    ContextCompat.getColor(this, R.color.red)
-                )
-
+                val newMessage =  getString(R.string.destination_input_status_outOfRange_message)
+                val newMessageColor = ContextCompat.getColor(this, R.color.red)
+                viewModel.messageText = newMessage
+                viewModel.messageColor = newMessageColor
+                binding.tvSetDestinationDescription.text = newMessage
+                binding.tvSetDestinationDescription.setTextColor(newMessageColor)
             }
             DestinationInputStatus.EMPTY -> {
-                binding.tvSetDestinationDescription.text = getString(
-                    R.string.destination_input_status_empty_message
-                )
-                binding.tvSetDestinationDescription.setTextColor(
-                    ContextCompat.getColor(this, R.color.red)
-                )
+                val newMessage = getString(R.string.destination_input_status_empty_message)
+                val newMessageColor = ContextCompat.getColor(this, R.color.red)
+                viewModel.messageText = newMessage
+                viewModel.messageColor = newMessageColor
+                binding.tvSetDestinationDescription.text = newMessage
+                binding.tvSetDestinationDescription.setTextColor(newMessageColor)
             }
             else -> {}
         }
